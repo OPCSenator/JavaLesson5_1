38faa99c7d0dfd854f44c10c82522a69e9076ac5
DrEvil <opc_senator4@mail.ru>
2022-10-23 23:36:36 +0300

    Поправил проверку условий на >= и <= т.к. по заданию границы входят в условия.

--- src/main/java/ru/metology/sqr/SQRService.java
+++ src/main/java/ru/metology/sqr/SQRService.java
@@ -9,7 +9,7 @@ public class SQRService {
         if (minValue > maxValue) { return -1;}
 
         for (int i = 10 ; i < 100 ; i++){
-            if (((i*i) > minValue) & ((i*i) < maxValue)) { mySqrCounts++;}
+            if (((i*i) >= minValue) & ((i*i) <= maxValue)) { mySqrCounts++;}
         }
         return mySqrCounts;
     }
